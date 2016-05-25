beaed44ba02b9028f6d2b33862043e0e92b925f3
Bryce Kerley <bkerley@brycekerley.net>
2016-05-25 16:11:25 -0400

    version bump

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,5 +1,13 @@
 # DocFirstFormatter Release Notes
 
+# 0.0.2
+
+A couple new features.
+
+* collects pending issues at end
+* fewer newlines
+* still no tests
+
 # 0.0.1
 
 Initial release.
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DocFirstFormatter.Mixfile do
 
   def project do
     [app: :doc_first_formatter,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      description: description,
      package: package,
